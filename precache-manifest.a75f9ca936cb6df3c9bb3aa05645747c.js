0508de62c2c502e79e12a2cc40190b240d58843e
Cris Vila <gz.vila@gmail.com>
2022-09-09 20:08:59 +0200

    Updates

---
+++ precache-manifest.a75f9ca936cb6df3c9bb3aa05645747c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65ba41c4a757b33ff2a51e52b5975a6b",
+    "url": "/stripes/index.html"
+  },
+  {
+    "revision": "ce210600ab77a702ba5f",
+    "url": "/stripes/static/css/main.9c0e5149.chunk.css"
+  },
+  {
+    "revision": "9d9ce4078b442a71cc60",
+    "url": "/stripes/static/js/2.e1e77988.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stripes/static/js/2.e1e77988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce210600ab77a702ba5f",
+    "url": "/stripes/static/js/main.86696989.chunk.js"
+  },
+  {
+    "revision": "61ec455a1c06039828d5",
+    "url": "/stripes/static/js/runtime-main.0dba3a1d.js"
+  },
+  {
+    "revision": "882a211ad0bae36b9c11715c9291f36b",
+    "url": "/stripes/static/media/hero.882a211a.svg"
+  },
+  {
+    "revision": "86d618b5be1508bcbbb31d6393f089f6",
+    "url": "/stripes/static/media/logo.86d618b5.svg"
+  },
+  {
+    "revision": "e908706c41907a9611ba9c4d61a32f7c",
+    "url": "/stripes/static/media/phone.e908706c.svg"
+  }
+]);
